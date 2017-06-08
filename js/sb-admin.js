f93db202a43b3e0310adcbc45d97d14b489ed5ba
Anders Cramer Nielsen <anders.cramer@me.com>
2017-06-08 20:06:04 +0200

    myAreaChart

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -38,10 +38,10 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+        labels: ["Jan", "Feb", "Mar", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"],
         datasets: [{
-            label: "Sessions",
-            lineTension: 0.3,
+            label: "Køb pr. måned",
+            lineTension: 0.2,
             backgroundColor: "rgba(2,117,216,0.2)",
             borderColor: "rgba(2,117,216,1)",
             pointRadius: 5,
@@ -49,9 +49,9 @@ var myLineChart = new Chart(ctx, {
             pointBorderColor: "rgba(255,255,255,0.8)",
             pointHoverRadius: 5,
             pointHoverBackgroundColor: "rgba(2,117,216,1)",
-            pointHitRadius: 20,
-            pointBorderWidth: 2,
-            data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+            pointHitRadius: 200,
+            pointBorderWidth: 1,
+            data: [100, 301, 262, 183, 182, 286],
         }],
     },
     options: {
@@ -64,13 +64,13 @@ var myLineChart = new Chart(ctx, {
                     display: false
                 },
                 ticks: {
-                    maxTicksLimit: 7
+                    maxTicksLimit: 12
                 }
             }],
             yAxes: [{
                 ticks: {
                     min: 0,
-                    max: 40000,
+                    max: 400,
                     maxTicksLimit: 5
                 },
                 gridLines: {

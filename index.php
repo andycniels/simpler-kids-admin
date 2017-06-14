e9a58b0112171d2447060d55c132eddd4796dff0
Casper Ragn <cr@simplar.dk>
2017-06-14 11:53:43 +0200

    thgfjgdj

--- index.php
+++ index.php
@@ -3,7 +3,26 @@ include 'header.php';
 ?>
             <!-- Icon Cards -->
             <div class="row">
-                <div class="col-xl-3 col-sm-6 mb-3">
+                 <!--<div class="col-xl-3 col-sm-6 mb-3">
+                    <div class="card card-inverse card-3 o-hidden h-100">
+                        <div class="card-block">
+                            <div class="card-block-icon">
+                                <i class="fa fa-recycle"></i>
+
+                            </div>
+                            <div class="mr-5">
+                                Abonnement status
+                            </div>
+                        </div>
+                        <a href="#" class="card-footer clearfix small z-1">
+                            <span class="float-left">Administrer Lager</span>
+                            <span class="float-right"><i class="fa fa-angle-right"></i></span>
+                        </a>
+                    </div>
+                </div>-->
+                   
+                    
+                 <div class="col-xl-3 col-sm-6 mb-3">
                     <div class="card card-inverse card-2 o-hidden h-100">
                         <div class="card-block">
                             <div class="card-block-icon">
@@ -21,39 +40,6 @@ include 'header.php';
                 </div>
                 
                 <div class="col-xl-3 col-sm-6 mb-3">
-                    <div class="card card-inverse card-1 o-hidden h-100">
-                        <div class="card-block">
-                            <div class="card-block-icon">
-                                <i class="fa fa-fw fa-comments"></i>
-                            </div>
-                            <div class="mr-5">
-                                26 New Messages!
-                            </div>
-                        </div>
-                        <a href="#" class="card-footer clearfix small z-1">
-                            <span class="float-left">View Details</span>
-                            <span class="float-right"><i class="fa fa-angle-right"></i></span>
-                        </a>
-                    </div>
-                </div>
-                
-                <div class="col-xl-3 col-sm-6 mb-3">
-                    <div class="card card-inverse card-3 o-hidden h-100">
-                        <div class="card-block">
-                            <div class="card-block-icon">
-                                <i class="fa fa-fw fa-shopping-cart"></i>
-                            </div>
-                            <div class="mr-5">
-                                123 New Orders!
-                            </div>
-                        </div>
-                        <a href="#" class="card-footer clearfix small z-1">
-                            <span class="float-left">View Details</span>
-                            <span class="float-right"><i class="fa fa-angle-right"></i></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-sm-6 mb-3">
                     <div class="card card-inverse card-4 o-hidden h-100">
                         <div class="card-block">
                             <div class="card-block-icon">
@@ -64,11 +50,31 @@ include 'header.php';
                             </div>
                         </div>
                         <a href="#" class="card-footer clearfix small z-1">
-                            <span class="float-left">View Details</span>
+                            <span class="float-left">Se mere</span>
                             <span class="float-right"><i class="fa fa-angle-right"></i></span>
                         </a>
                     </div>
                 </div>
+                
+                <div class="col-xl-3 col-sm-6 mb-3">
+                    <div class="card card-inverse card-1 o-hidden h-100">
+                        <div class="card-block">
+                            <div class="card-block-icon">
+                                <i class="fa fa-fw fa-comments"></i>
+                            </div>
+                            <div class="mr-5">
+                                Beskeder
+                            </div>
+                        </div>
+                        <a href="#" class="card-footer clearfix small z-1">
+                            <span class="float-left">Læs nye beskeder</span>
+                            <span class="float-right"><i class="fa fa-angle-right"></i></span>
+                        </a>
+                    </div>
+                </div>
+                
+              
+                
             </div>
             
             <!-- Example Bar Chart Card -->
@@ -103,7 +109,7 @@ include 'header.php';
             <!-- Area Chart Example -->
             <div class="card mb-3">
                 <div class="card-header">
-                    <i class="fa fa-area-chart"></i> Area Chart Example
+                    <i class="fa fa-area-chart"></i> Antal abonnenter
                 </div>
                 <div class="card-block">
                     <canvas id="myAreaChart" width="100%" height="30"></canvas>
